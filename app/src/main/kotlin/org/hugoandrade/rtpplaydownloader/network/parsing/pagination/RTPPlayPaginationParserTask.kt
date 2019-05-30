d30ea68c31c3cbb43a0a2050d4943b2300911f18
Hugo Andrade <hugo.mf.andrade@gmail.com>
2019-05-30 20:43:34 +0100

    able to request "show more" javascript function

--- app/src/main/kotlin/org/hugoandrade/rtpplaydownloader/network/parsing/pagination/RTPPlayPaginationParserTask.kt
+++ app/src/main/kotlin/org/hugoandrade/rtpplaydownloader/network/parsing/pagination/RTPPlayPaginationParserTask.kt
@@ -2,6 +2,7 @@ package org.hugoandrade.rtpplaydownloader.network.parsing.pagination
 
 import org.hugoandrade.rtpplaydownloader.utils.NetworkUtils
 import org.jsoup.Jsoup
+import org.jsoup.nodes.DataNode
 import org.jsoup.nodes.Document
 import java.net.SocketTimeoutException
 
@@ -67,6 +68,35 @@ class RTPPlayPaginationParserTask : PaginationParserTaskBase() {
                     return paginationUrl
                 }
 
+                // temp
+
+                val stamp = findHtmlOfLastClass(doc, "last_id")//: 234238
+                val listDate = findInScript(doc, "RTPPLAY.currentHPListDate").replace("\"", "")//:
+                val listQuery = findInScript(doc, "RTPPLAY.currentHPListQuery").replace("\"", "")//:
+                val listProgram = findInScript(doc, "RTPPLAY.currentHPListProgram").replace("\"", "")//: 2383
+                val listcategory = findInScript(doc, "RTPPLAY.currentHPListCategory").replace("\"", "")//:
+                val listchannel = findInScript(doc, "RTPPLAY.currentHPListChannel").replace("\"", "")//:
+                val listtype = findInScript(doc, "RTPPLAY.currentHPList").replace("\"", "")//: recent
+                val page = findInScript(doc, "RTPPLAY.currentHPPageList").replace("\"", "")//: 2
+                val type = findInScript(doc, "RTPPLAY.currentHPListType").replace("\"", "")//: tv
+                val currentItemSelected = findInScript(doc, "RTPPLAY.currentItemSelected").replace("\"", "")//: 236098
+
+                val req = "https://www.rtp.pt/play/bg_l_ep/?" +
+                        "stamp=" + stamp + "&" +
+                        "listDate=" + listDate + "&" +
+                        "listQuery=" + listQuery + "&" +
+                        "listProgram=" + listProgram + "&" +
+                        "listcategory=" + listcategory + "&" +
+                        "listchannel=" + listchannel + "&" +
+                        "listtype=" + "recent" + "&" +
+                        "page=" + page + "&" +
+                        "type=" + type + "&" +
+                        "currentItemSelected=" + currentItemSelected
+
+                android.util.Log.e(TAG, "req = " + req)
+
+                // end temp
+
                 val episodeItems = doc?.getElementsByClass("episode-item")
 
                 if (episodeItems != null && !episodeItems.isEmpty()) {
@@ -107,4 +137,45 @@ class RTPPlayPaginationParserTask : PaginationParserTaskBase() {
 
         return paginationUrl
     }
+
+    private fun findInScript(doc: Document, id: String): String {
+
+        val scriptElements = doc.getElementsByTag("script")
+
+        if (scriptElements != null) {
+
+            for (scriptElement in scriptElements.iterator()) {
+
+                for (dataNode: DataNode in scriptElement.dataNodes()) {
+
+                    if (dataNode.wholeData.contains(id + " = ")) {
+                        return extractValue(dataNode, "$id = ", ";")
+                    }
+                }
+            }
+        }
+        return ""
+    }
+
+    private fun extractValue(dataNode: DataNode, start: String, end: String): String {
+        val first = dataNode.wholeData.substring(dataNode.wholeData.indexOf(start) + start.length)
+        val second = first.substring(0, first.indexOf(end))
+        return second
+    }
+
+    private fun findHtmlOfLastClass(doc: Document, className: String): String {
+
+        val classElements = doc.getElementsByClass(className)
+
+        if (classElements != null) {
+
+            val classElement = classElements.last()
+
+            if (classElement != null) {
+                return classElement.html()
+            }
+        }
+        return ""
+    }
 }
+
