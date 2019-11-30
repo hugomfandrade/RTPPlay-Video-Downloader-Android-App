39741ba993a5d8dc955cc2ccf2395b075068177f
Hugo Andrade <hugo.mf.andrade@gmail.com>
2019-11-30 18:55:30 +0000

    thumbnail of images

--- app/src/main/kotlin/org/hugoandrade/rtpplaydownloader/network/download/SICDownloaderTask.kt
+++ app/src/main/kotlin/org/hugoandrade/rtpplaydownloader/network/download/SICDownloaderTask.kt
@@ -5,6 +5,7 @@ import org.hugoandrade.rtpplaydownloader.utils.NetworkUtils
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 import java.net.SocketTimeoutException
 import java.net.URL
 
@@ -13,8 +14,9 @@ class SICDownloaderTask : DownloaderTaskBase() {
     override fun parseMediaFile(url: String): Boolean {
 
         this.url = url
-        mediaUrl = getVideoFile(url) ?: return false
-        filename = MediaUtils.getUniqueFilenameAndLock(getMediaFileName(url, mediaUrl))
+        this.mediaUrl = getVideoFile(url) ?: return false
+        this.filename = MediaUtils.getUniqueFilenameAndLock(getMediaFileName(url, mediaUrl))
+        this.thumbnailUrl = getThumbnailPath(url)
 
         try {
             URL(mediaUrl)
@@ -140,5 +142,48 @@ class SICDownloaderTask : DownloaderTaskBase() {
         }
 
         return videoFile?:urlString
+    }
+
+    private fun getThumbnailPath(urlString: String): String? {
+        try {
+            val doc: Document
+
+            try {
+                doc = Jsoup.connect(urlString).timeout(10000).get()
+            } catch (ignored: SocketTimeoutException) {
+                return null
+            }
+
+            val headElements = doc.getElementsByTag("head")?:return null
+
+            for (headElement in headElements.iterator()) {
+
+                val metaElements = headElement.getElementsByTag("meta")?: Elements()
+
+                for (metaElement in metaElements.iterator()) {
+
+                    if (!metaElement.hasAttr("property") ||
+                            !metaElement.hasAttr("name") ||
+                            !metaElement.hasAttr("content") ||
+                            metaElement.attr("name") != "twitter:image" ||
+                            metaElement.attr("property") != "og:image") {
+                        continue
+                    }
+
+                    val thumbnail = metaElement.attr("content")
+                    if (thumbnail.isNullOrEmpty()) {
+                        continue
+                    }
+                    else {
+                        return thumbnail
+                    }
+                }
+            }
+        }
+        catch (e : java.lang.Exception) {
+            e.printStackTrace()
+        }
+
+        return null
     }
 }
